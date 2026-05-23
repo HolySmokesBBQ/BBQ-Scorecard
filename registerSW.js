af170ce347f9b64f3c5c44ee73a2f7b35b7f67a2
AndyReidJr <jmuiller@hotmail.com>
2026-05-23 15:43:29 -0500

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/BBQ-Scorecard/sw.js', { scope: '/BBQ-Scorecard/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
